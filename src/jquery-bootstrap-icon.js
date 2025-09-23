c8c30cb3d00fa9db8f13b79d6fe373be7c8befba
Niels Holt <niels@steenbuchholt.dk>
2025-09-24 00:17:33 +0200

    No message

--- src/jquery-bootstrap-icon.js
+++ src/jquery-bootstrap-icon.js
@@ -8,10 +8,10 @@ jquery-bootstrap-icon.js
     $.extend({
         /******************************************************
         $.bsIcon( icon, colorName )
-        Return a [] with classes for a icon in color = Bootstrap alert colorName ('success'
+        Return a [] with classes for a icon in color = Bootstrap alert colorName ('success', 'warning', 'alert', 'error', 'ingo', 'help'...)
         ******************************************************/
         bsIcon: function( icon, colorName ){
-            return ['fas '+icon+' BACK-TEXT-COLOR-'+colorName, $.FONTAWESOME_PREFIX +' '+icon+' FRONT-TEXT-COLOR-'+colorName];
+            return [['fas '+icon+' BACK-TEXT-COLOR-'+colorName, $.FONTAWESOME_PREFIX +' '+icon+' FRONT-TEXT-COLOR-'+colorName]];
         },
     });
 
